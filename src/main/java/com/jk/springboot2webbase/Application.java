bb2049c2eae93a74f4b88c6e82965b3aa1c90a99
jk_hoap <7385697@qq.com>
2019-09-26 21:58:22 +0800

    Initial commit

---
+++ src/main/java/com/jk/springboot2webbase/Application.java
@@ -0,0 +1,20 @@
+package com.jk.springboot2webbase;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @ClassName: com.jk.springboot2webbase
+ * @Auther: jk
+ * @Date: 2019-09-26 21:39
+ * @Description: springboot2-web-base
+ * Date      Author        Version        Description
+ * 2019-09-26     jk               v1.0                   新增
+ */
+@SpringBootApplication
+public class Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+}
